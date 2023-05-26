28fae9f7391f0fe2e2a7c5aca93c0296e21ea5fc
Luvis <corzoluvis@gmail.com>
2023-05-25 23:43:31 -0300

    Ejercicio 9 LuvisC

--- ejercicio9/ejercicio9POO/src/Servicios/ServicioMatematica.java
+++ ejercicio9/ejercicio9POO/src/Servicios/ServicioMatematica.java
@@ -5,10 +5,59 @@
  */
 package Servicios;
 
-/**
- *
- * @author daiay
- */
+import Entidad.Matematica;
+import java.util.Scanner;
+
+
 public class ServicioMatematica {
+    
+    Scanner leer = new Scanner(System.in);
+    Matematica num = new Matematica();
+    
+    
+    public Matematica numeros(){
+        
+       
+        num.setNumero1((Double)(Math.random()*10+1));
+        num.setNumero2((Double)(Math.random()*10+1));
+        System.out.println("Primer numero: " + num.getNumero1());
+        System.out.println("Segundo numero: " + num.getNumero2());
+        return num;
+        
+     
+    }
+    
+    public double devolverMayor(){
+        
+        double mayor = Math.max(num.getNumero1(), num.getNumero2());
+       // System.out.println("El numero mayor es: " + mayor);
+        
+       /** if (mayor == num.getNumero2()){
+            System.out.println("El numero2 es mayor: ");
+        } else {
+            System.out.println("_____________________");
+            System.out.println("El numero1 es el mayor");
+        }
+        */ return mayor;    
+    }
+    
+    public double calcularPotencia(){
+        
+    double menor = Math.min(num.getNumero1(), num.getNumero2());
+    double potencia = Math.pow(Math.round(devolverMayor()), Math.round(menor));
+       // System.out.println(Math.round(devolverMayor()));
+       // System.out.println(Math.round(menor));
+       System.out.println("La potencia del numero mayor es: " + potencia);
+        
+        return menor;
+        
+    }
+    
+    public double calculaRaiz(){
+        
+      double raiz = Math.sqrt(Math.abs(Math.round(calcularPotencia())));
+        System.out.println("La raiz es: " + raiz);
+        return raiz;
+    }
     
 }
